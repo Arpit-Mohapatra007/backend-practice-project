4eb757f133d93298ae6c9e366a591ee54fbe8799
Arpit Mohapatra <arpitmohapatra06@gmail.com>
2025-12-18 14:11:18 +0530

    Sub-Pipelining

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,7 @@ import jwt from "jsonwebtoken";
 
 import fs from "fs"
 import { subscribe } from "diagnostics_channel";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId)=>{
     try {
@@ -371,3 +372,54 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
 })
 
 export {getUserChannelProfile}
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(
+                    req.user._id
+                )
+            },
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200)
+        .json(
+            new ApiResponse(200,user[0].watchHistory,"Watch History Fetched Successfully !!")
+        )
+})
+
+export {getWatchHistory}
